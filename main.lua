8329c0865ef065741995492a49f09cc9740e636a
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-09-10 16:04:34 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,13 @@ local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/scripts/bloxfruit.lua"))()
   	end
-end
+
+--[[ WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk! ]]
+
+local plr = game:GetService("Players").LocalPlayer
+
+getgenv().Anti = true -- Re-Execute if you change it
+
 local Anti
 Anti = hookmetamethod(game, "__namecall", function(self, ...)
         if self == plr and getnamecallmethod():lower() == "kick" and getgenv().Anti then
