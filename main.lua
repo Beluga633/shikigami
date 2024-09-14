07dacfa55398adecbf414084ebfa762b5fed9dcc
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-09-14 20:03:29 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+getgenv().Team = "Marines" -- Pirates/Marines
+getgenv().Fix_Lag = true -- true/false
+getgenv().Auto_Execute = false -- true/false
+getgenv().Clear_Settings = false -- true/false
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
