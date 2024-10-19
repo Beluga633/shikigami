247fd825a1dd58df14a66cecf8db83a8b7de8eed
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-10-19 21:59:59 +0800

    Add files via upload

---
+++ data/arisgado7.lua
@@ -0,0 +1,10 @@
+getgenv().Team = "Marine" -- Pirates/Marines
+getgenv().Fix_Lag = true -- true/false
+getgenv().Auto_Execute = false -- true/false
+getgenv().Auto_Rejoin = true -- true/false
+getgenv().Clear_Settings = false -- true/false
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/scripts/bloxfruit.lua"))()
+end
