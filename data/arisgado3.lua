1016520e996b069fd0a8bd3644fb40565d8c0d34
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-10-19 21:41:40 +0800

    Update arisgado3.lua

--- data/arisgado3.lua
+++ data/arisgado3.lua
@@ -1,1 +1,9 @@
-
+getgenv().Team = "Marine" -- Pirates/Marines
+getgenv().Fix_Lag = true -- true/false
+getgenv().Auto_Execute = false -- true/false
+getgenv().Auto_Rejoin = true -- true/false
+getgenv().Clear_Settings = false -- true/false
+local Player = game.Players.LocalPlayer
+if Player.UserId == 3573965288
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/scripts/bloxfruit.lua"))()
+end
