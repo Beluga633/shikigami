b680f7a7476b6865b0c8df24213dcc4f65793d74
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-10-20 21:14:33 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,3 @@
-local whitelistUserIDs = {3573965288,2577131658,281820166,2526634520,7137421404,361824180,5734902543,3545179610} -- Use player ids as its more secure if a player was to change their username.
-
-game.Players.PlayedAdded:Connect(function(player)
-    if not table.find(whitelistUserIDs,player.UserId) then --If the UserID value is not in the table this returns nil. In Lua nil equals false.
-        player:Kick("You are not whitelisted!.")
-    end
-end)
-
 repeat wait() until game:IsLoaded()
 local Player = game.Players.LocalPlayer
 if Player.UserId == 3573965288 then
