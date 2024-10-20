15fec6966aebf2ea5acddf204fdf99d7869d917c
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-10-20 19:13:34 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
 repeat wait() until game:IsLoaded()
 local Player = game.Players.LocalPlayer
-if Player.UserId == 3573965288
+if Player.UserId == 3573965288 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/arisgado3.lua"))()
-elseif Player.UserId == 2577131658
+elseif Player.UserId == 2577131658 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/arisgado7.lua"))()
-elseif Player.UserId == 2818201667
+elseif Player.UserId == 2818201667 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/glignacio54321.lua"))()
-elseif Player.UserId == 2526634520
+elseif Player.UserId == 2526634520 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/kierbermio.lua"))()
-elseif Player.UserId == 7137421404
+elseif Player.UserId == 7137421404 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/kieryos05.lua"))()
-elseif Player.UserId == 3618241806
+elseif Player.UserId == 3618241806 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/mucoyco123.lua"))()
-elseif Player.UserId == 5734902543
+elseif Player.UserId == 5734902543 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/cemberkyle001.lua"))()
-elseif Player.UserId == 3545179610
+elseif Player.UserId == 3545179610 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/Beluga633/shikigami/main/data/bobosigawa.lua"))()
 end
