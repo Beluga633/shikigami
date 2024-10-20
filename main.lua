ec1d503e21740e031fb200e0f8a8c4b35cd36f6e
Beluga633 <112786100041@r6-1.deped.gov.ph>
2024-10-20 19:56:11 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local whitelistUserIDs = {3573965288,2577131658,281820166,2526634520,7137421404,
 
 game.Players.PlayedAdded:Connect(function(player)
     if not table.find(whitelistUserIDs,player.UserId) then --If the UserID value is not in the table this returns nil. In Lua nil equals false.
-        player:Kick("You are not whitelisted on this server.")
+        player:Kick("You are not whitelisted!.")
     end
 end)
 
